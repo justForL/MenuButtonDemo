62ab9482a377729a0ac59f617077b274f0eb279e
James <layer_james@163.com>
2016-07-13 11:23:08 +0800

    完成动画效果

--- MenuButton/MenuButton.m
+++ MenuButton/MenuButton.m
@@ -15,7 +15,7 @@
 @end
 
 @implementation MenuButton
-
+#pragma mark - 初始化
 - (instancetype)init {
     if (self = [super init]) {
         self.backgroundColor = [UIColor redColor];
@@ -28,14 +28,14 @@
     }
     return self;
 }
-
+#pragma mark - 创建内部button
 - (void)setupButtons {
     for (int i = 0; i<4; i++) {
         UIButton *button = [[UIButton alloc]init];
         button.tag = i;
         button.frame = self.frame;
-//        button.hidden = YES;
-//        button.alpha = 0;
+        button.hidden = YES;
+        button.alpha = 0;
         button.backgroundColor = [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
         
         button.layer.cornerRadius = 25;
@@ -46,43 +46,77 @@
     }
     [self.superview bringSubviewToFront:self];
 }
+#pragma mark - 暴露在外的初始化类方法
 + (instancetype)creatMenuButton {
     return [[self alloc]init];
 }
-
+#pragma mark - 圆形按钮已经显示到父视图调用的方法
 - (void)didMoveToSuperview {
     [self setupButtons];
 }
+#pragma mark - 拖拽按钮的事件
 - (void)panActionWithGesture:(UIPanGestureRecognizer *)recognizer{
    CGPoint point = [recognizer locationInView:self.superview];
-    self.center = point;
     NSLog(@"%@",NSStringFromCGPoint(point));
-    for (UIButton *button in self.buttonArray) {
-        button.frame = self.frame;
+        self.center = point;
+        for (UIButton *button in self.buttonArray) {
+                button.frame = self.frame;
+                button.alpha = 0;
+                button.hidden = YES;
+        }
+}
+#pragma mark - 前置红色按钮点击事件
+- (void)buttonClick:(UIButton *)sender {
+    
+    sender.selected = !sender.selected;
+    if (sender.selected) {
+        [self openButtonState];
+    }else {
+        [self closeButonOpenState];
     }
     
-    
 }
-
-- (void)buttonClick:(UIButton *)sender {
-    NSLog(@"%s",__func__);
+#pragma mark - 后置按钮的打开状态
+- (void)openButtonState {
     CGFloat margin = 70;
     for (int i = 0; i< self.buttonArray.count; i++) {
         UIButton *button = self.buttonArray[i];
-        if (i == 0) {
-            button.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y - margin, button.frame.size.width, button.frame.size.height);
-        }else {
-            UIButton *prebutton = self.buttonArray[i-1];
-            button.frame = CGRectMake(button.frame.origin.x, prebutton.frame.origin.y - margin, button.frame.size.width, button.frame.size.height);
-        }
+        [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:0.3 initialSpringVelocity:0.6 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            button.hidden = NO;
+            button.alpha = 1;
+            if (i == 0) {
+                button.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y - margin, button.frame.size.width, button.frame.size.height);
+            }else {
+                UIButton *prebutton = self.buttonArray[i-1];
+                button.frame = CGRectMake(button.frame.origin.x, prebutton.frame.origin.y - margin, button.frame.size.width, button.frame.size.height);
+            }
+        } completion:^(BOOL finished) {
+            
+        }];
     }
 }
+#pragma mark - 后置按钮的关闭状态
+- (void)closeButonOpenState {
+    [UIView animateWithDuration:0.25 animations:^{
+        for (UIButton *button in self.buttonArray) {
+            
+            button.frame = self.frame;
+        }
+    } completion:^(BOOL finished) {
+        for (UIButton *button in self.buttonArray) {
+            button.hidden = YES;
+            button.alpha = 0;
+        }
+    }];
+}
+#pragma mark - 后置按钮的点击事件
 - (void)backButtonClick:(UIButton *)sender {
     if (self.clickBlock) {
         self.clickBlock(sender.tag);
+        [self closeButonOpenState];
     }
 }
-
+#pragma mark - lazyLoad用于缓存所有后置按钮
 - (NSMutableArray *)buttonArray {
     if (_buttonArray == nil) {
         _buttonArray = [NSMutableArray array];
